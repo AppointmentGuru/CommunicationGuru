56a4f6d4762e4ed1299cfdbe578904792fcb9a7d
toast38coza <info@38.co.za>
2017-09-28 13:12:57 +0200

    Add communicationstatus to admin

--- api/admin.py
+++ api/admin.py
@@ -2,7 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import CommunicationTemplate, Communication
+from .models import CommunicationTemplate, Communication, CommunicationStatus
 
 admin.site.register(Communication)
+admin.site.register(CommunicationStatus)
 admin.site.register(CommunicationTemplate)
--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -13,4 +13,3 @@ class WebHookTestCase(TestCase):
         }
         result = self.client.post('/incoming/slack/', json.dumps(data), content_type='application/json')
         assert result.status_code == 200
-        import ipdb;ipdb.set_trace()
